3488eca866dcf7937c690b74f0d7f26f3c99b489
Laurens Offereins <lmoffereins@gmail.com>
2020-09-12 00:19:29 +0200

    Rebuild event listeners when triggers are enabled or disabled

--- qs-emergo-triggers.js
+++ qs-emergo-triggers.js
@@ -238,7 +238,7 @@ define([
 	 *
 	 * @type {Array}
 	 */
-	monitoredEventProps = ["event", "field", "value", "state", "eitherOr"],
+	monitoredEventProps = ["event", "field", "value", "state", "eitherOr", "enabled"],
 
 	/**
 	 * Determine which triggers should be added or removed
